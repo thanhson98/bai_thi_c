83c2c409c43b7955a177ab4055d8bfb4027790c5
thanh son <you@example.com>
2018-10-22 20:25:35 +0700

    bai-tap

---
+++ cmake-build-debug/CMakeFiles/bai_thi_c_program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bai_thi_c_program.dir/main.c.obj"
+  "bai_thi_c_program.pdb"
+  "bai_thi_c_program.exe"
+  "bai_thi_c_program.exe.manifest"
+  "libbai_thi_c_program.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bai_thi_c_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/bai_thi_c_program.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for bai_thi_c_program.
+# This may be replaced when dependencies are built.
